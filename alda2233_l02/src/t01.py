740b9c82389045a2f673d12499f73386f28abc8e
mohamed223567 <131572320+mohamed223567@users.noreply.github.com>
2023-09-16 14:47:27 -0400

    test

--- alda2233_l02/src/t01.py
+++ alda2233_l02/src/t01.py
@@ -5,7 +5,7 @@
 Author:  Mohamed
 ID:      169072233
 Email:   alda2233@mylaurier.ca
-__updated__ = "2023-09-11"
+__updated__ = "2023-09-16"
 -------------------------------------------------------
 """
 # Imports
@@ -32,3 +32,4 @@ FREEZING = 32
 celsius = int(input("Input temperature in degrees celsius"))
 fahrenheit = int(9/5*(celsius)+32)
 print(f"Temperature (F):{fahrenheit}")
+print("THIS IS A TEST")
